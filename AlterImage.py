3f411e5d13d36913790173f04135ecd5a9863454
bjesse93 <bpara001@ucr.edu>
2019-05-22 13:15:10 -0700

    -rebase

--- AlterImage.py
+++ AlterImage.py
@@ -5,20 +5,27 @@ from scipy import misc
 def imageShower(image):
     plt.figure()
     plt.imshow(image)
+    plt.axis('off')
+    plt.gcf()
+    plt.savefig('img3.png', dpi=100)
     plt.show()
 
 def imageShowerGrey(image):
     plt.gray()
     plt.imshow(image)
+    plt.axis('off')
+    plt.gcf()
+    plt.savefig('img4.png', dpi=100)
     plt.show()
 
 def main():
     ascent = scipy.misc.ascent()
 
     img2= scipy.misc.ascent()
-    img3=gaussian_filter(img2,sigma=5)
+    img3=gaussian_filter(img2,sigma=2)
 
     imageShower(img3)
+
     imageShowerGrey(img2)
 
 
