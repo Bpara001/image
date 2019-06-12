6819560f96f1329684299a8d5ecc7fb243fcf5c1
Bpara001 <bpara001@ucr.edu>
2019-06-11 21:05:50 -0700

    -filter applied; needs to colored

--- AlterImage.py
+++ AlterImage.py
@@ -1,11 +1,22 @@
 import scipy.misc
 from scipy.ndimage import gaussian_filter
 import matplotlib.pyplot as plt
+import os,glob
+from os import path
 from scipy import misc
-def imageShower(image):
+import matplotlib.image as mpimg
+from PIL import Image
+from IPython.display import set_matplotlib_formats
+
+def imageShower(image,i):
     plt.figure()
+    plt.axis('off')
     plt.imshow(image)
-    plt.show()
+    temp= str(i)
+    file_gauss= temp+'gauss.jpg'
+    plt.savefig(path.join('Gauss2/',file_gauss), bbox_inches= 'tight', pad_inches= 0)
+
+
 
 def imageShowerGrey(image):
     plt.gray()
@@ -13,13 +24,17 @@ def imageShowerGrey(image):
     plt.show()
 
 def main():
-    ascent = scipy.misc.ascent()
 
-    img2= scipy.misc.ascent()
-    img3=gaussian_filter(img2,sigma=5)
+    path = r'C:\Users\Jesse\PycharmProjects\image\Pictures\Casia2\Au'
+    i = 0
+    for filename in glob.glob(os.path.join(path,'*.jpg')):
+        with open(filename, 'r') as f:
+            print(filename)
+            image=mpimg.imread(filename)
+            alter=gaussian_filter(image,sigma=5)
+            imageShower(alter,i)
+            i =i+1
 
-    imageShower(img3)
-    imageShowerGrey(img2)
 
 
 
