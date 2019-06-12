02c236533ada147258761a82e1275089e7b97b6d
Bpara001 <bpara001@ucr.edu>
2019-06-12 10:18:35 -0700

    -gaussian filter script

--- AlterImage.py
+++ AlterImage.py
@@ -1,27 +1,24 @@
 import scipy.misc
 from scipy.ndimage import gaussian_filter
 import matplotlib.pyplot as plt
-import os,glob
+import os,glob,cv2
 from os import path
 from scipy import misc
+from scipy.ndimage import gaussian_filter
 import matplotlib.image as mpimg
 from PIL import Image
 from IPython.display import set_matplotlib_formats
+def rgb2gray(rgb):
+    return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
 
+import numpy as np
 def imageShower(image,i):
-    plt.figure()
+    fig=plt.figure()
     plt.axis('off')
     plt.imshow(image)
-    temp= str(i)
-    file_gauss= temp+'gauss.jpg'
-    plt.savefig(path.join('Gauss2/',file_gauss), bbox_inches= 'tight', pad_inches= 0)
-
-
-
-def imageShowerGrey(image):
-    plt.gray()
-    plt.imshow(image)
-    plt.show()
+    temp = str(i)
+    file_gauss = temp + 'gauss.jpg'
+    plt.savefig(path.join('Gauss2/', file_gauss),bbox_inches='tight',pad_inches=0)
 
 def main():
 
@@ -29,13 +26,14 @@ def main():
     i = 0
     for filename in glob.glob(os.path.join(path,'*.jpg')):
         with open(filename, 'r') as f:
-            print(filename)
-            image=mpimg.imread(filename)
-            alter=gaussian_filter(image,sigma=5)
+            #print(filename)
+            image = (mpimg.imread(filename))
+            print(image.shape)
+            alter = gaussian_filter(image, sigma=2)
             imageShower(alter,i)
             i =i+1
 
-
+#gaussian_filter(image,sigma=5)
 
 
 if __name__ == "__main__":
